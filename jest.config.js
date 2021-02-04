30a3caa5c69919ec46973ee40e04aaf8ed25317a
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-02-04 09:17:46 +0000

    chore: migrate to pnpm

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,24 @@
 const { pathsToModuleNameMapper } = require('ts-jest/utils')
 const { compilerOptions } = require('./tsconfig.base.json')
 
-const esModules = ['lit-element', 'lit-html'].join('|');
+const esModules = ['lit-element', 'lit-html']
 
 module.exports = {
   testMatch: ['**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)']
 , transform: {
     '^.+\\.ts$': 'ts-jest'
-  , [`(${esModules}).+\\.js$`]: 'babel-jest'
+  , [`(${concat(esModules)}).+\\.js$`]: 'babel-jest'
   }
-, transformIgnorePatterns: [`/node_modules/(?!${esModules})`]
+, transformIgnorePatterns: [`node_modules/(?!${concatForPnpm(esModules)})`]
 , moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/'
   })
+}
+
+function concat(modules) {
+  return modules.join('|')
+}
+
+function concatForPnpm(modules) {
+  return concat(modules.map(x => `(.*${x})`))
 }
