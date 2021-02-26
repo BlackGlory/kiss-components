4c6b2ed8fa9e3bf7d95296c594c4d8d0085dd1b5
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-02-26 22:59:35 +0800

    chore: migrate to yarn

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,16 @@
 const { pathsToModuleNameMapper } = require('ts-jest/utils')
 const { compilerOptions } = require('./tsconfig.base.json')
 
-const esModules = ['lit-element', 'lit-html']
+const esModules = ['lit-element', 'lit-html'].join('|');
 
 module.exports = {
   testMatch: ['**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)']
 , transform: {
     '^.+\\.ts$': 'ts-jest'
-  , [`(${concat(esModules)}).+\\.js$`]: 'babel-jest'
+  , [`(${esModules}).+\\.js$`]: 'babel-jest'
   }
-, transformIgnorePatterns: [`node_modules/(?!${concatForPnpm(esModules)})`]
+, transformIgnorePatterns: [`/node_modules/(?!${esModules})`]
 , moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/'
   })
-}
-
-function concat(modules) {
-  return modules.join('|')
-}
-
-function concatForPnpm(modules) {
-  return concat(modules.map(x => `(.*${x})`))
 }
